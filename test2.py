035dc492286660c0853f632e9a550b91b7eab5b7
孫苡菱 <d0782838@mail.fcu.edu.tw>
2020-05-24 10:40:47 +0800

    tkinter

--- test2.py
+++ test2.py
@@ -1,22 +1,32 @@
-import kivy
-kivy.require('1.10.0')
-from kivy.app import App
-from kivy.uix.label import Label
-from kivy.lang import Builder
+import tkinter as tk
 
-Builder.load_string('''
-<SimpleLabel>:
-    text: 'Hello World!!!'
-''')
+window = tk.Tk() #建立主視窗window
+window.title('board')
+window.geometry('400x300')
+window.configure(background='white')
+
+top_frame = tk.Frame(window) #建立top群
+top_frame.pack()
+bottom_frame = tk.Frame(window) #建立bottom群
+bottom_frame.pack(side=tk.BOTTOM)
+# 建立事件處理函式
+def echo_hello():
+    result = "查詢結果等等..."
+    result_label.configure(text=result)
+
+left_button = tk.Button(top_frame, text='球員數據', fg='red')
+left_button.pack(side=tk.LEFT)
+
+middle_button = tk.Button(top_frame, text='球隊數據', fg='green')
+middle_button.pack(side=tk.LEFT)
+
+right_button = tk.Button(top_frame, text='排行查詢', fg='blue')
+right_button.pack(side=tk.LEFT)
 
 
-class SimpleLabel(Label):
-    pass
+result_label = tk.Button(bottom_frame, text='', fg='black')
+result_label.pack(side=tk.BOTTOM)
+bottom_button = tk.Button(bottom_frame, text='查詢', fg='black', command=echo_hello)
+bottom_button.pack(side=tk.BOTTOM)
 
-
-class SampleApp(App):
-    def build(self):
-        return SimpleLabel()
-
-if __name__ == "__main__":
-    SampleApp().run()
+window.mainloop()
