9932fcfec1b023d5f80904de0dc86a48e6343d7c
孫苡菱 <d0782838@mail.fcu.edu.tw>
2020-05-30 09:10:35 +0800

    新增紀錄版面

--- write_board.py
+++ write_board.py
@@ -1,38 +1,64 @@
 import tkinter as tk
 
-window = tk.Tk() #建立視窗window
-window.title('write board')
-window.geometry('500x300') #window大小
+class RecordBoard(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self._frame = None
+        self.geometry('500x300')
+        self.switch_frame(StartPage)
 
-def do_print():
-    print('game name: %s' %(gameString.get))
-    print('opponent school: %s' %(oppschoolString.get))
-    print('opponent department: %s' %(oppdepString.get))
+    def switch_frame(self, frame_class):
+        new_frame = frame_class(self)
+        if self._frame is not None:
+            self._frame.destroy()
+        self._frame = new_frame
+        self._frame.pack()
 
-#Label
-gameLabel = tk.Label(window, text='盃賽名稱:')
-oppschoolLabel = tk.Label(window, text='對手學校')
-oppdepLabel = tk.Label(window, text='對手系名')
+class StartPage(tk.Frame):
+    def __init__(self, master):
 
-gameLabel.grid(column=0, row=0, sticky=tk.W)
-oppschoolLabel.grid(column=0, row=1, sticky=tk.W)
-oppdepLabel.grid(column=0, row=2, sticky=tk.W)
+        def do_print():
+            print('%s' %(dateString.get()))
+            print('%s' %(gameString.get()))
+            print('%s' %(oppschoolString.get()))
+            print('%s' %(oppdepString.get()))
 
-#Entry輸入
-gameString = tk.StringVar()
-oppschoolString = tk.StringVar()
-oppdepString = tk.StringVar()
-gameEntry = tk.Entry(window, show=None, font=('Arial', 14), textvariable=gameString)
-oppschoolEntry = tk.Entry(window, show=None, font=('Arial', 14), textvariable=oppschoolString)
-oppdepEntry = tk.Entry(window, show=None, font=('Arial', 14), textvariable=oppdepString)
+        tk.Frame.__init__(self, master)
+        tk.Label(self, text="開始記錄", font=('Arial', 18, "bold")).grid(column=0, row=0, sticky=tk.W)
+        #Label-文字標籤
+        dateLabel = tk.Label(self, text='日期:')
+        gameLabel = tk.Label(self, text='盃賽名稱:')
+        oppschoolLabel = tk.Label(self, text='對手學校')
+        oppdepLabel = tk.Label(self, text='對手系名')
 
-gameEntry.grid(column=1, row=0, padx=10)
-oppschoolEntry.grid(column=1, row=1, padx=10)
-oppdepEntry.grid(column=1, row=2, padx=10)
+        dateLabel.grid(column=0, row=1, sticky=tk.W)
+        gameLabel.grid(column=0, row=2, sticky=tk.W)
+        oppschoolLabel.grid(column=0, row=3, sticky=tk.W)
+        oppdepLabel.grid(column=0, row=4, sticky=tk.W)
+        #定義文字輸入框裡的文字物件
+        dateString = tk.StringVar()
+        gameString = tk.StringVar()
+        oppschoolString = tk.StringVar()
+        oppdepString = tk.StringVar()
+        dateEntry = tk.Entry(self, show=None, font=('Arial', 14), textvariable=dateString)
+        gameEntry = tk.Entry(self, show=None, font=('Arial', 14), textvariable=gameString)
+        oppschoolEntry = tk.Entry(self, show=None, font=('Arial', 14), textvariable=oppschoolString)
+        oppdepEntry = tk.Entry(self, show=None, font=('Arial', 14), textvariable=oppdepString)
 
-#提交Button
-pushButton = tk.Button(window, text='提交', command=do_print)
+        dateEntry.grid(column=1, row=1, padx=10)
+        gameEntry.grid(column=1, row=2, padx=10)
+        oppschoolEntry.grid(column=1, row=3, padx=10)
+        oppdepEntry.grid(column=1, row=4, padx=10)
 
-pushButton.grid(column=0, row=3, pady=10, sticky=tk.W)
+        tk.Button(self, text='確定', command=lambda: [master.switch_frame(Page_ChoosePlayer), do_print()]).grid(column=0, row=5, sticky=tk.W)
 
-window.mainloop() #主視窗迴圈顯示
+class Page_ChoosePlayer(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        tk.Frame.configure(self, bg='red')
+        tk.Label(self, text="選擇5個上場球員", font=('Arial', 18, "bold")).pack(side="top", fill="x", pady=5)
+        tk.Button(self, text="Go back to start page", command=lambda: master.switch_frame(StartPage)).pack()
+
+if __name__ == "__main__":
+    window = RecordBoard()
+    window.mainloop()
