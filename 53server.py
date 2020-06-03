e197f77088bb541bd26513f395971fecaff38a7d
D0782868 <samantha890809@gmail.com>
2020-06-03 23:18:12 +0800

    111

--- 53server.py
+++ 53server.py
@@ -1,7 +1,6 @@
 #-*-coding:UTF-8 -*-
 import pymysql
 
-sql=' '
 conn = pymysql.connect(
     host='localhost',
     user='yuan',
@@ -9,3 +8,17 @@ conn = pymysql.connect(
     database='testdb'
 )
 cursor = conn.cursor()
+
+def new_data():
+    sql = "INSERT INTO 球員( 名字, 學號, 背號, 入隊學年 )VALUES('孫乙玲', 'D0782838', 10, '107')"
+    try:
+        cursor.execute(sql,())
+    # 執行SQL语句
+   # 提交到資料庫系統執行
+        conn.commit()
+        print("insert a record into temp")
+    except:
+   # 發生異常錯誤時回復
+        conn.rollback()
+    
+new_data()
