ecc41dbdbb2b2cb2fa48f58ba9f491244a11ecfc
孫苡菱 <d0782838@mail.fcu.edu.tw>
2020-10-29 06:22:25 +0800

    登入

--- login.py
+++ login.py
@@ -1,5 +1,14 @@
 import server
 
+while(1):
+    id = input('NID：')
+    search_st = server.find_student()
+    search_te = server.find_teacher()
+    for key, value in (d for d in search_st):
+        if key == 'NID':
+            if id == value:
+                print('Welcome ' + id)
+                break
+    print('No Account ! Register or Try again !')
 
-id = input('NID：')
 
