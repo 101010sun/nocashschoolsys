5235c6e7b8ba9b06d16d0fa68dca717b09b335e5
孫苡菱 <d0782838@mail.fcu.edu.tw>
2020-10-29 06:52:50 +0800

    註冊頁面

--- register.py
+++ register.py
@@ -1,21 +1,26 @@
 import server
 
+print('-------- REGISTER PAGE --------')
 identity = int(input('你的身分是(1.老師 2.學生): '))
-if(identity==1):
+#register teacher's account
+if(identity == 1):
     nid = str(input('NID: '))
     name = str(input('姓名: '))
     department = str(input('系所: '))
+
     server.insert_teacher(nid, name, department)
-    server.find_teacher()
+    print('Register Successful !')
     
-elif(identity==2):
-    nid = input('NID: ')
-    name = input('姓名: ')
-    department = input('系所: ')
-    grade = input('年級: ')
-    sex = input('性別: ')
-    residence = input('居住地: ')
+#register student's account   
+elif(identity == 2):
+    nid = str(input('NID: '))
+    name = str(input('姓名: '))
+    department = str(input('系所: '))
+    grade = int(input('年級: '))
+    sex = str(input('性別(Man\Woman): '))
+    residence = str(input('居住地: '))
+
     server.insert_student(nid, name, department, grade, sex, residence)
-    server.find_student()
+    print('Register Successful !')
         
   
