ea3030b95b6bd414b580e784e199a486a6282937
孫苡菱 <d0782838@mail.fcu.edu.tw>
2020-10-29 07:10:38 +0800

    防呆

--- register.py
+++ register.py
@@ -1,5 +1,6 @@
 import server
 
+flag = 0
 print('-------- REGISTER PAGE --------')
 identity = int(input('你的身分是(1.老師 2.學生): '))
 #register teacher's account
@@ -7,9 +8,15 @@ if(identity == 1):
     nid = str(input('NID: '))
     name = str(input('姓名: '))
     department = str(input('系所: '))
-
-    server.insert_teacher(nid, name, department)
-    print('Register Successful !')
+    #protect repeat register
+    search_te = server.find_teacher()
+    for d in search_te:
+        if d['NID'] == nid:
+            print('You had registered ! Please LOGIN')
+            flag = 1
+    if(flag == 0): 
+        server.insert_teacher(nid, name, department)
+        print('Register Successful !')
     
 #register student's account   
 elif(identity == 2):
@@ -19,8 +26,15 @@ elif(identity == 2):
     grade = int(input('年級: '))
     sex = str(input('性別(Man/Woman): '))
     residence = str(input('居住地: '))
+    #protect repeat register
+    search_st = server.find_student()
+    for d in search_st:
+        if d['NID'] == nid:
+            print('You had registered ! Please LOGIN')
+            flag = 1
+    if(flag == 0): 
+        server.insert_student(nid, name, department, grade, sex, residence)
+        print('Register Successful !')
 
-    server.insert_student(nid, name, department, grade, sex, residence)
-    print('Register Successful !')
         
   
