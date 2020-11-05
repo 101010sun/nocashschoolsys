e74506fbd7f5fe74d468f928fd85a262ef7c41b2
孫苡菱 <d0782838@mail.fcu.edu.tw>
2020-11-05 14:41:21 +0800

    show web

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-# render_template :樣式模板使用 HTML 撰寫，後台處理使用 Python
-# server.py       :連接資料庫
-# request         :request請求來與HTML互動
 from flask import Flask, render_template, url_for, request, session, redirect
 from flask_pymongo import PyMongo
 import server
@@ -20,17 +17,17 @@ def index():
         return 'You are logged in as ' + session['NID']
     return render_template('Home_page.html')
 
-#login
-@app.route('/login', methods=['POST'])
+
+@app.route('/login', methods=['GET','POST'])
 def login():
-    users = mongo.db.student
-    login_user = users.find_one({'NID': request.form['NID']})
-    
-    if login_user:
-        if bcrypt.hashpw(request.form['NID'].encode('utf-8'), login_user['NID'].encode('utf-8')) == login_user['NID'].encode('utf-8'):
-            session['NID'] = request.form['NID']
+    if request.method == 'POST':
+        users = mongo.db.student
+        login_user = users.find_one({'NID': request.form['NID']})
+        if login_user is not None:
+            #session['NID'] = request.form['NID']
             return redirect(url_for('index'))
-    return 'Invalid NID combination'
+        return 'Invalid NID combination'
+    return render_template('login.html')
 
 @app.route('/register', methods=['Get', 'POST'])
 def register():
@@ -39,16 +36,8 @@ def register():
         existing_user = users.find_one({'NID': request.form['NID']})
 
         if existing_user is None:
-            # print(request.form['NID'])
-            # print(request.form['Name'])
-            # print(request.form['Dept'])
-            # print(request.form['Average'])
-            # print(request.form['Rank'])
-            # print(request.form['Grade'])
-            # print(request.form['Sex'])
-            # print(request.form['Residence'])
             server.insert_student(request.form['NID'], request.form['Name'], request.form['Dept'], int(request.form['Grade']), float(request.form['Average']), int(request.form['Rank']), request.form['Sex'], request.form['Residence'])
-            #session['NID'] = request.form['NID']
+            # session['NID'] = request.form['NID']
             return redirect(url_for('index'))
 
         return 'That NID already exists!'
