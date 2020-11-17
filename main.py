ff58e5e7b26123eb237269a9d7011f0c70005b9d
孫苡菱 <d0782838@mail.fcu.edu.tw>
2020-11-18 02:55:23 +0800

    change

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ def login():
         isteacher = teacher.find_one({'NID': nid})
         if isstudent is not None or isteacher is not None:
             session['username'] = nid
-            return redirect(url_for('index'))
-        return redirect(url_for('index'))
+            return render_template('home_page.html')
+        return render_template('login.html')
     return redirect(url_for('index'))
 
 @app.route('/register', methods=['Get', 'POST'])
@@ -43,17 +43,23 @@ def register():
         isteacher = teacher.find_one({'NID': nid})
         if isstudent is None and 'D' in nid:
             session['username'] = nid
-            return render_template('register_student.html')
+            return redirect(url_for('reg_student'))
         elif isteacher is None and 'T' in nid:
             session['username'] = nid
-            return render_template('register_teacher.html')
+            return redirect(url_for('reg_student'))
         return 'That NID already exists!'
 
     return render_template('register.html')
 
 @app.route('/register/student', methods=['Get', 'POST'])
-def register_student():
-    
+def reg_student():
+    if request.method == 'POST':
+        nid = session.get('username')
+        if nid is not None and 'D' in nid:
+            server.insert_student(nid, request.form['Name'], request.form['Dept'], request.form['Grade'], request.form['Average'], request.form['Rank'], request.form['Sex'], request.form['Residence'])
+        # elif nid is not None and 'T' in nid:
+        #     server.insert_teacher(nid, request.form['Name'], request.form['Dept'])
+    return 'fail'
 
 
 if __name__ == "__main__":
