3dd15373577e7c8b1dd25ce2a7300f76438cc672
孫苡菱 <d0782838@mail.fcu.edu.tw>
2020-11-18 00:13:39 +0800

    session

--- main.py
+++ main.py
@@ -2,20 +2,22 @@ from flask import Flask, render_template, url_for, request, session, redirect
 from flask_pymongo import PyMongo
 import server
 import bcrypt
+import os
 
 app = Flask(__name__)
 # open debugger
 app.config['DEBUG'] = True
 app.config['MONGO_DBNAME'] = 'nocashschoolsys'
 app.config['MONGO_URI'] = 'mongodb://localhost:27017/nocashschoolsys'
+app.config['SECRET_KEY'] = os.urandom(24)
 
 mongo = PyMongo(app)
 
 @app.route('/')
 def index():
-    if 'NID' in session:
-        return 'You are logged in as ' + session['NID']
-    return render_template('Home_page.html')
+    if 'username' in session:
+        return 'You are logged in as ' + session['username']
+    return render_template('login.html')
 
 
 @app.route('/login', methods=['GET','POST'])
@@ -24,10 +26,10 @@ def login():
         users = mongo.db.student
         login_user = users.find_one({'NID': request.form['NID']})
         if login_user is not None:
-            #session['NID'] = request.form['NID']
+            session['username'] = request.form['NID']
             return redirect(url_for('index'))
-        return 'Invalid NID combination'
-    return render_template('login.html')
+        return ''
+    return redirect(url_for('index'))
 
 @app.route('/register', methods=['Get', 'POST'])
 def register():
@@ -37,7 +39,7 @@ def register():
 
         if existing_user is None:
             server.insert_student(request.form['NID'], request.form['Name'], request.form['Dept'], int(request.form['Grade']), float(request.form['Average']), int(request.form['Rank']), request.form['Sex'], request.form['Residence'])
-            # session['NID'] = request.form['NID']
+            session['username'] = request.form['NID']
             return redirect(url_for('index'))
 
         return 'That NID already exists!'
